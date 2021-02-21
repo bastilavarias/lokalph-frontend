8dbac09d911ded7f5d97ef3fbc90c14bd9ca28b5
Sebastian <denzilbao@gmail.com>
2021-02-21 13:01:28 +0800

    Set up frontend (app-customer-client) ecosystem.

---
+++ src/main.js
@@ -0,0 +1,15 @@
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import Buefy from "buefy";
+import "@/styles/index.scss";
+
+Vue.config.productionTip = false;
+Vue.use(Buefy);
+
+new Vue({
+    router,
+    store,
+    render: (h) => h(App),
+}).$mount("#app");
