058b3633ed74324c2924f67caf05f1f5382aa65e
Sebastian <denzilbao@gmail.com>
2021-02-27 12:00:47 +0800

    Change frontend framework of app-client-customer from buefy to vuetify.

--- src/main.js
+++ src/main.js
@@ -1,15 +1,14 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
-import Buefy from "buefy";
-import "@/styles/index.scss";
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import vuetify from './plugins/vuetify';
 
-Vue.config.productionTip = false;
-Vue.use(Buefy);
+Vue.config.productionTip = false
 
 new Vue({
-    router,
-    store,
-    render: (h) => h(App),
-}).$mount("#app");
+  router,
+  store,
+  vuetify,
+  render: h => h(App)
+}).$mount('#app')
