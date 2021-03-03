49fec25dc454bb8d680cda5f1223289b1081c865
Sebastian <denzilbao@gmail.com>
2021-03-03 16:42:02 +0800

    save progress.

--- src/components/custom/places-component.vue
+++ src/components/custom/places-component.vue
@@ -1,102 +1,41 @@
 <template>
-    <div>
-        <v-autocomplete
-            :search-input.sync="search"
-            :items="searchResults"
-            :outlined="outlined"
-            :label="label"
-            append-icon="mdi-map-marker-outline"
-            item-text="description"
-            item-value="place_id"
-            autocomplete="new"
-            clearable
-            @click:clear="placeId = null"
-            hint="We use Google's places autocomplete to find your address."
-            persistent-hint
-            v-model="placeId"
-        ></v-autocomplete>
-    </div>
+    <vuetify-algolia-places
+        v-model="placeLocal"
+        language="en"
+        :outlined="outlined"
+        :label="label"
+        append-icon="mdi-map-marker"
+        debounce
+        clearable
+        @click:clear="placeLocal = null"
+        autocomplete="new"
+    ></vuetify-algolia-places>
 </template>
 
 <script>
 export default {
     name: "custom-places-component",
 
-    data() {
-        return {
-            placeId: null,
-            search: null,
-            searchResults: [],
-            service: null,
-        };
-    },
-
     props: {
         outlined: {
             type: Boolean,
             required: false,
         },
-
         label: {
             type: String,
             required: false,
         },
     },
 
-    metaInfo() {
+    data() {
         return {
-            script: [
-                {
-                    src: `https://maps.googleapis.com/maps/api/js?key=${process.env.VUE_APP_GOOGLE_SECRET_KEY}&libraries=places`,
-                    async: true,
-                    defer: true,
-                    callback: () => this.MapsInit(),
-                },
-            ],
+            placeLocal: null,
         };
     },
 
-    methods: {
-        MapsInit() {
-            this.service = new window.google.maps.places.AutocompleteService();
-        },
-        displaySuggestions(predictions, status) {
-            if (status !== window.google.maps.places.PlacesServiceStatus.OK) {
-                this.searchResults = [];
-                return;
-            }
-            this.searchResults = predictions;
-        },
-    },
-
     watch: {
-        search(value) {
-            if (!value) return (this.searchResults = []);
-            if (value) {
-                this.service.getPlacePredictions(
-                    {
-                        input: this.search,
-                        componentRestrictions: { country: "ph" },
-                        types: [],
-                    },
-                    this.displaySuggestions
-                );
-            }
-        },
-
-        async placeId(value) {
-            // const url = `https://maps.googleapis.com/maps/api/place/details/json?placeid=${value}&key=${process.env.VUE_APP_GOOGLE_SECRET_KEY}`;
-            // const request = {
-            //     placeId: value,
-            // };
-            // this.service.getDetails(request, function (place, status) {
-            //     if (
-            //         status !== window.google.maps.places.PlacesServiceStatus.OK
-            //     ) {
-            //         return;
-            //     }
-            //     console.log(place);
-            // });
+        placeLocal(value) {
+            console.log(value);
         },
     },
 };
--- src/main.js
+++ src/main.js
@@ -5,10 +5,17 @@ import store from "./store";
 import vuetify from "./plugins/vuetify";
 import axiosService from "@/services/axios-service";
 import VueMeta from "vue-meta";
+import VuetifyAlgoliaPlaces from "vuetify-algolia-places";
 
 Vue.config.productionTip = false;
 axiosService.init();
 Vue.use(VueMeta);
+Vue.use(VuetifyAlgoliaPlaces, {
+    algolia: {
+        appId: "plN18NJ07I8D",
+        apiKey: "f693daecd680aeb18bb06c39b59dfbee",
+    },
+});
 
 new Vue({
     router,
