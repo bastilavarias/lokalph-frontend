1c9664c8908c19c6fa68154ca9d1d3b9db98792a
Sebastian <denzilbao@gmail.com>
2021-03-09 12:35:25 +0800

    save progress.

--- src/views/seller-dashboard/shop/Form.vue
+++ src/views/seller-dashboard/shop/Form.vue
@@ -1,56 +1,53 @@
 <template>
-    <v-dialog width="480" persistent v-model="isOpenLocal">
-        <v-card>
-            <v-card-title>
-                <span>Create a new Shop</span>
-                <v-spacer></v-spacer>
-                <v-btn icon @click="isOpenLocal = false">
-                    <v-icon>mdi-close</v-icon>
-                </v-btn>
-            </v-card-title>
-            <v-card-subtitle>Lorem ipsum dolor sit amet.</v-card-subtitle>
-            <v-card-text>
-                <v-row dense>
-                    <v-col cols="12" v-if="isErrorAlertOpen">
-                        <v-alert type="error">
-                            {{ errorAlertMessage }}
-                        </v-alert>
-                    </v-col>
-                    <v-col cols="12">
-                        <span class="subtitle-2">Shop Information</span>
-                    </v-col>
-                    <v-col cols="12">
-                        <v-text-field
-                            label="Name"
-                            outlined
-                            v-model="form.name"
-                        ></v-text-field>
-                    </v-col>
-                    <v-col cols="12">
-                        <v-textarea
-                            outlined
-                            label="About the Shop"
-                            v-model="form.introduction"
-                        ></v-textarea>
-                    </v-col>
-                    <v-col cols="12">
-                        <span class="subtitle-2">Shop Contact</span>
-                    </v-col>
-                    <v-col cols="12">
-                        <custom-places-component
-                            outlined
-                            label="Address"
-                            :place.sync="form.address"
-                        ></custom-places-component>
-                    </v-col>
-                    <v-col cols="12">
-                        <v-text-field
-                            label="Contact Number"
-                            outlined
-                            type="tel"
-                            v-model="form.contactNumber"
-                        ></v-text-field>
-                    </v-col>
+    <v-card outlined>
+        <v-card-title>
+            <span>Create a new Shop</span>
+            <v-spacer></v-spacer>
+        </v-card-title>
+        <v-card-subtitle>Lorem ipsum dolor sit amet.</v-card-subtitle>
+        <v-card-text>
+            <v-row dense>
+                <v-col cols="12" v-if="isErrorAlertOpen">
+                    <v-alert type="error">
+                        {{ errorAlertMessage }}
+                    </v-alert>
+                </v-col>
+                <v-col cols="12">
+                    <span class="subtitle-2">Shop Information</span>
+                </v-col>
+                <v-col cols="12">
+                    <v-text-field
+                        label="Name"
+                        outlined
+                        v-model="form.name"
+                    ></v-text-field>
+                </v-col>
+                <v-col cols="12">
+                    <v-textarea
+                        outlined
+                        label="About the Shop"
+                        v-model="form.introduction"
+                    ></v-textarea>
+                </v-col>
+                <v-col cols="12">
+                    <span class="subtitle-2">Shop Contact</span>
+                </v-col>
+                <v-col cols="12">
+                    <custom-places-component
+                        outlined
+                        label="Address"
+                        :place.sync="form.address"
+                    ></custom-places-component>
+                </v-col>
+                <v-col cols="12">
+                    <v-text-field
+                        label="Contact Number"
+                        outlined
+                        type="tel"
+                        v-model="form.contactNumber"
+                    ></v-text-field>
+                </v-col>
+                <template v-if="!doesUserHasStripe">
                     <v-col cols="12">
                         <div class="d-flex justify-space-between align-center">
                             <span class="subtitle-2">Stripe Details</span>
@@ -73,30 +70,30 @@
                             outlined
                             v-model="form.publishableKey"
                         ></v-text-field>
+                    </v-col>
+                    <v-col cols="12">
                         <v-text-field
                             label="Secret Key"
                             outlined
                             v-model="form.secretKey"
                         ></v-text-field>
                     </v-col>
-                    <v-col cols="12">
-                        <v-btn
-                            color="primary"
-                            block
-                            depressed
-                            @click="createShop"
-                            :loading="isCreateShopStart"
-                        >
-                            <span class="text-capitalize mr-2"
-                                >Create Shop</span
-                            >
-                            <v-icon>mdi-storefront</v-icon>
-                        </v-btn>
-                    </v-col>
-                </v-row>
-            </v-card-text>
-        </v-card>
-    </v-dialog>
+                </template>
+                <v-col cols="12">
+                    <v-btn
+                        color="primary"
+                        block
+                        depressed
+                        @click="createShop"
+                        :loading="isCreateShopStart"
+                    >
+                        <span class="text-capitalize mr-2">Create Shop</span>
+                        <v-icon>mdi-storefront</v-icon>
+                    </v-btn>
+                </v-col>
+            </v-row>
+        </v-card-text>
+    </v-card>
 </template>
 
 <script>
@@ -114,18 +111,10 @@ const defaultForm = {
 };
 
 export default {
-    name: "profile-shop-create-form-dialog-component",
     components: { CustomPlacesComponent },
-    props: {
-        isOpen: {
-            type: Boolean,
-            required: true,
-        },
-    },
 
     data() {
         return {
-            isOpenLocal: this.isOpen,
             form: Object.assign({}, defaultForm),
             defaultForm,
             isCreateShopStart: false,
@@ -138,23 +127,10 @@ export default {
         user() {
             return this.$store.state.authentication.user;
         },
-    },
 
-    watch: {
-        isOpen(value) {
-            this.isOpenLocal = value;
+        doesUserHasStripe() {
+            return this.user.stripe;
         },
-
-        isOpenLocal(value) {
-            this.$emit("update:isOpen", value);
-        },
-    },
-
-    mounted() {
-        if (this.user.stripe) {
-            this.form.publishableKey = this.user.stripe.publishable_key;
-            this.form.secretKey = this.user.stripe.secret_key;
-        }
     },
 
     methods: {
@@ -182,11 +158,9 @@ export default {
                 return;
             }
             if (success) {
-                console.log(data);
                 this.isErrorAlertOpen = false;
                 this.errorAlertMessage = null;
                 this.isCreateShopStart = false;
-                this.isOpenLocal = false;
                 this.$store.commit(GLOBAL_SET_SNACKBAR_CONFIGS, {
                     isOpen: true,
                     text: success_message,
