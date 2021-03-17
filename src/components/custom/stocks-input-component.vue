e2b35d3a4582ee0c475ca86fb5eadb2f483b0280
Sebastian <denzilbao@gmail.com>
2021-03-17 13:48:59 +0800

    save progress.

---
+++ src/components/custom/stocks-input-component.vue
@@ -0,0 +1,63 @@
+<template>
+    <div class="d-flex justify-space-between">
+        <v-btn
+            x-large
+            color="primary"
+            depressed
+            tile
+            height="56"
+            @click="stocksLocal--"
+        >
+            <v-icon>mdi-minus</v-icon>
+        </v-btn>
+        <v-text-field
+            label="Stocks"
+            outlined
+            :style="{ borderRadius: '0' }"
+            class="centered-input"
+            type="number"
+            v-model="stocksLocal"
+        ></v-text-field>
+        <v-btn
+            x-large
+            color="primary"
+            depressed
+            tile
+            height="56"
+            @click="stocksLocal++"
+        >
+            <v-icon>mdi-plus</v-icon>
+        </v-btn>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "custom-stocks-input-component",
+
+    data() {
+        return {
+            stocksLocal: 1,
+        };
+    },
+
+    watch: {
+        stocksLocal(value) {
+            if (typeof value === "string") {
+                this.stocksLocal = 1;
+            }
+            if (value < 1) {
+                this.stocksLocal = 1;
+            }
+        },
+    },
+};
+</script>
+
+<style scoped>
+.centered-input >>> input {
+    text-align: center;
+    font-weight: bold;
+    font-size: 1.6rem;
+}
+</style>
--- src/views/seller-dashboard/product/Form.vue
+++ src/views/seller-dashboard/product/Form.vue
@@ -10,6 +10,33 @@
                         operation="create"
                     ></custom-image-input-component>
                 </v-col>
+                <v-col cols="12">
+                    <v-text-field
+                        outlined
+                        label="Title"
+                        :counter="100"
+                    ></v-text-field>
+                </v-col>
+                <v-col cols="12">
+                    <v-textarea
+                        outlined
+                        label="Description"
+                        :counter="400"
+                    ></v-textarea>
+                </v-col>
+                <v-col cols="12">
+                    <v-autocomplete outlined label="Category"></v-autocomplete>
+                </v-col>
+                <v-col cols="12">
+                    <v-text-field
+                        type="number"
+                        outlined
+                        label="Price per unit"
+                    ></v-text-field>
+                </v-col>
+                <v-col cols="12">
+                    <custom-stocks-input-component></custom-stocks-input-component>
+                </v-col>
             </v-row>
         </v-card-text>
         <v-card-actions>
@@ -23,7 +50,12 @@
 <script>
 import SellerDashboardFormCardComponent from "@/components/global/seller-dashboard-content-card-component";
 import CustomImageInputComponent from "@/components/custom/image-input-component";
+import CustomStocksInputComponent from "@/components/custom/stocks-input-component";
 export default {
-    components: { CustomImageInputComponent, SellerDashboardFormCardComponent },
+    components: {
+        CustomStocksInputComponent,
+        CustomImageInputComponent,
+        SellerDashboardFormCardComponent,
+    },
 };
 </script>
