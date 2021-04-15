8825a52bcfd0a6c4a55f0be0dac7b5271e5b49d1
Sebastian <denzilbao@gmail.com>
2021-04-15 23:28:48 +0800

    save progress.

--- src/views/customer-dashboard/Chat.vue
+++ src/views/customer-dashboard/Chat.vue
@@ -57,53 +57,83 @@
                     </v-list-item-action>
                 </v-list-item>
                 <v-divider></v-divider>
+                <div
+                    :style="{ height: '430px', overflow: 'auto' }"
+                    ref="messages"
+                >
+                    <v-card-text>
+                        <v-row dense>
+                            <template v-for="(chat, index) in chats">
+                                <v-col cols="12" :key="index">
+                                    <template v-if="chat.sender === 0">
+                                        <div
+                                            :style="{ maxWidth: '75%' }"
+                                            class="d-flex"
+                                        >
+                                            <v-avatar :size="25" class="mr-2">
+                                                <v-img
+                                                    src="https://res.cloudinary.com/deqllunb9/image/upload/v1618281994/lokal-ph-development/products/5f903103e5300231c292e547a17c4918_ht2bux.jpg"
+                                                ></v-img>
+                                            </v-avatar>
+                                            <div>
+                                                <!--Implement truncate here-->
+                                                <span
+                                                    class="caption"
+                                                    title="Sebastian Curtis Lavarias"
+                                                >
+                                                    Sebastian Curtis Lavarias
+                                                </span>
+                                                <v-card
+                                                    class="my-1"
+                                                    color="#E4E6EB"
+                                                    shaped
+                                                    flat
+                                                >
+                                                    <v-card-text
+                                                        class="black--text"
+                                                    >
+                                                        Lorem ipsum dolor sit
+                                                        amet.
+                                                    </v-card-text>
+                                                </v-card>
+                                            </div>
+                                        </div>
+                                    </template>
+                                    <template v-if="chat.sender === 1">
+                                        <div
+                                            class="d-flex justify-space-between"
+                                        >
+                                            <v-spacer></v-spacer>
+                                            <v-card
+                                                color="secondary lighten--5"
+                                                flat
+                                                max-width="75%"
+                                            >
+                                                <v-card-text
+                                                    class="white--text"
+                                                >
+                                                    {{ chat.message }}
+                                                </v-card-text>
+                                            </v-card>
+                                        </div>
+                                    </template>
+                                </v-col>
+                            </template>
+                        </v-row>
+                    </v-card-text>
+                </div>
+
                 <v-card-text>
-                    <div :style="{ height: '450px', overflow: 'auto' }">
-                        <div :style="{ maxWidth: '75%' }" class="d-flex">
-                            <v-avatar :size="25" class="mr-2">
-                                <v-img
-                                    src="https://res.cloudinary.com/deqllunb9/image/upload/v1618281994/lokal-ph-development/products/5f903103e5300231c292e547a17c4918_ht2bux.jpg"
-                                ></v-img>
-                            </v-avatar>
-                            <div>
-                                <!--Implement truncate here-->
-                                <span
-                                    class="caption"
-                                    title="Sebastian Curtis Lavarias"
-                                >
-                                    Sebastian Curtis Lavarias
-                                </span>
-                                <v-card
-                                    class="my-1"
-                                    color="#E4E6EB"
-                                    shaped
-                                    flat
-                                >
-                                    <v-card-text class="black--text">
-                                        Lorem ipsum dolor sit amet.
-                                    </v-card-text>
-                                </v-card>
-                            </div>
-                        </div>
-                        <div class="d-flex justify-space-between">
-                            <v-spacer></v-spacer>
-                            <v-card
-                                color="secondary lighten--5"
-                                flat
-                                max-width="75%"
-                            >
-                                <v-card-text class="white--text">
-                                    test
-                                </v-card-text>
-                            </v-card>
-                        </div>
-                    </div>
                     <v-textarea
                         outlined
                         height="80"
                         color="secondary"
                         placeholder="Type your message here..."
                         no-resize
+                        v-model="message"
+                        :counter="200"
+                        @keyup.enter="sendChat"
+                        autofocus
                     >
                         <template v-slot:append-outer>
                             <v-btn
@@ -112,6 +142,8 @@
                                 small
                                 depressed
                                 class="align-self-center"
+                                :disabled="!isFormValid"
+                                @click="sendChat"
                             >
                                 <v-icon>mdi-send</v-icon>
                             </v-btn>
@@ -128,7 +160,45 @@ export default {
     data() {
         return {
             room: null,
+            chats: [],
+            message: null,
         };
+    },
+
+    computed: {
+        isFormValid() {
+            const message = this.message ? this.message.trim() : null;
+            return message
+                ? message.length >= 2 && message.length <= 200
+                : !!message;
+        },
+    },
+
+    methods: {
+        scrollToElement() {
+            const element = this.$refs.messages;
+            if (element) element.scrollIntoView({ behavior: "smooth" });
+        },
+
+        sendChat() {
+            console.log(this.isFormValid);
+            if (this.isFormValid) {
+                this.chats = [
+                    ...this.chats,
+
+                    {
+                        message: "Test",
+                        sender: 0,
+                    },
+
+                    {
+                        message: this.message.trim(),
+                        sender: 1,
+                    },
+                ];
+                this.message = null;
+            }
+        },
     },
 };
 </script>
