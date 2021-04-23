d22f23341b5c72b31d4d92c8d14ab5a14fd2e425
Sebastian <denzilbao@gmail.com>
2021-04-23 15:00:49 +0800

    save progress.

--- src/repositories/chat-repository.js
+++ src/repositories/chat-repository.js
@@ -1,7 +1,10 @@
 import axiosService from "@/services/axios-service";
 
 const chatRepository = {
-    async initiateChat(payload) {},
+    async initiateChat(formData) {
+        const response = await this.axios.post("/chat/room", formData);
+        return response.data;
+    },
 };
 
 export default chatRepository;
