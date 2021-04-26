a0f5d035f3c12b6a1bbb12ff4b1d5f1bf8695860
Sebastian <denzilbao@gmail.com>
2021-04-26 12:16:15 +0800

    save progress.

--- src/main.js
+++ src/main.js
@@ -21,7 +21,6 @@ Vue.use(VuetifyAlgoliaPlaces, {
 });
 Vue.use(InfiniteLoading);
 Vue.use(CKEditor);
-console.log(process.env.VUE_APP_PUSHER_APP_KEY);
 Vue.use(pusher, {
     api_key: process.env.VUE_APP_PUSHER_APP_KEY,
     options: {
