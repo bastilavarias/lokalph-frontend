f087865af7cf3a59a1915119dcaed9eb0d0a0ef2
Sebastian <denzilbao@gmail.com>
2021-05-04 13:09:17 +0800

    save progress.

--- src/repositories/product-repository.js
+++ src/repositories/product-repository.js
@@ -91,8 +91,11 @@ const shopRepository = {
         return response.data;
     },
 
-    async createProductOffer(payload) {
-        const response = await axiosService.post("/product/offer", payload);
+    async searchProducts({ page, per_page, search }) {
+        const url = `/product/search?page=${page}${
+            per_page ? `&per_page=${per_page}` : ""
+        }${search ? `&search=${search}` : ""}`;
+        const response = await axiosService.get(url);
         return response.data;
     },
 };
